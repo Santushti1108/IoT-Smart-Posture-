c11795145889443e52e1755729c0acbf86a35920
Santushti Singh <singhsantushti111@gmail.com>
2026-02-20 13:02:44 +0530

    changed http to https

--- Client/Posture/src/api/flexApi.js
+++ Client/Posture/src/api/flexApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://iot-smart-posture-backend.onrender.com/api/flex";
+const BASE_URL = "https://iot-smart-posture-backend.onrender.com/api/flex";
 
 export const getAllFlexData = async () => {
   const res = await axios.get(`${BASE_URL}/all`);
